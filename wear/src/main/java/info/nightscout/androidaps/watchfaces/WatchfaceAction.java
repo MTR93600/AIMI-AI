9dce4b8b75e79be46686a53b59191d00ae429c7d
Philoul <legoupil.jp@free.fr>
2020-01-05 15:21:36 +0100

    Watchface improvements : 5 customized zones for direct tap actions from watchfaces Color improvement in graph (Carb color orange, Bg value in range light green) Color for BG value in range (light green in dark watchface, dark green in bright watchface) AAPSv2 Adjust text size according to watch resolution

---
+++ wear/src/main/java/info/nightscout/androidaps/watchfaces/WatchfaceAction.java
@@ -0,0 +1,16 @@
+package info.nightscout.androidaps.watchfaces;
+
+public enum WatchfaceAction {
+    MENU,
+    WIZARD,
+    BOLUS,
+    ECARB,
+    TEMPT,
+    CPP,
+    PUMP,
+    TDD,
+    LOOP,
+    STATUS,
+    NONE
+}
+
