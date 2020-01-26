d41a7e05d5c5afcb920686ebf6dfbbf4993d87b8
Philoul <legoupil.jp@free.fr>
2020-01-26 20:55:10 +0100

    Steampunk watchface changeChartTimeframe and onTapCommand removed cutomized DOWN zone replaced by change Chart time frame resizePointSize true (specific variable previously added in BaseWatchFace for Steampunk watchface)

--- wear/src/main/java/info/nightscout/androidaps/watchfaces/Steampunk.java
+++ wear/src/main/java/info/nightscout/androidaps/watchfaces/Steampunk.java
@@ -29,33 +29,19 @@ public class Steampunk extends BaseWatchFace {
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         layoutView = inflater.inflate(R.layout.activity_steampunk, null);
         performViewSetup();
+        resizePointSize = true;
+        chartOk = true;
     }
 
     @Override
-    protected void onTapCommand(int tapType, int x, int y, long eventTime) {
-
-        if (tapType == TAP_TYPE_TAP&&
-                x >= mChartTap.getLeft() &&
-                x <= mChartTap.getRight()&&
-                y >= mChartTap.getTop() &&
-                y <= mChartTap.getBottom()){
-            if (eventTime - chartTapTime < 800){
-                changeChartTimeframe();
-            }
-            chartTapTime = eventTime;
-
-        } else if (tapType == TAP_TYPE_TAP&&
-                x >= mMainMenuTap.getLeft() &&
-                x <= mMainMenuTap.getRight()&&
-                y >= mMainMenuTap.getTop() &&
-                y <= mMainMenuTap.getBottom()){
-            if (eventTime - mainMenuTapTime < 800){
-                Intent intent = new Intent(this, MainMenuActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-            mainMenuTapTime = eventTime;
-        }
+    public void getTapZones(){
+        // tap zones for direct actions
+        tapxlow = mRelativeLayout.getWidth()/3;
+        tapylow = mRelativeLayout.getHeight()/3;
+        tapcharttop = 2*tapylow;
+        tapchartbottom = mRelativeLayout.getHeight();   // disable DOWN action
+        tapchartleft = tapxlow;
+        tapchartright = 2*tapxlow;
     }
 
     @Override
@@ -255,15 +241,4 @@ public class Steampunk extends BaseWatchFace {
         }
     }
 
-    private void changeChartTimeframe() {
-        int timeframe = Integer.parseInt(sharedPrefs.getString("chart_timeframe", "3"));
-        timeframe = (timeframe%5) + 1;
-        if (timeframe < 3) {
-            pointSize = 2;
-        } else {
-            pointSize = 1;
-        }
-        setupCharts();
-        sharedPrefs.edit().putString("chart_timeframe", "" + timeframe).commit();
-    }
 }
