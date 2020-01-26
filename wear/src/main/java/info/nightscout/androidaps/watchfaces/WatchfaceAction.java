88075f934a7f923d197355ae0f8db53247a87f5d
Philoul <legoupil.jp@free.fr>
2020-01-26 16:05:01 +0100

    New enum files for WatchfaceZone and WatchfaceAction

---
+++ wear/src/main/java/info/nightscout/androidaps/watchfaces/WatchfaceAction.java
@@ -0,0 +1,16 @@
+package info.nightscout.androidaps.watchfaces;
+
+public enum WatchfaceAction {
+    MAINMENU,
+    WIZARD,
+    BOLUS,
+    ECARB,
+    TEMPT,
+    CPP,
+    PUMP,
+    TDD,
+    LOOP,
+    STATUS,
+	SETTINGS,
+    NONE
+}
---
+++ wear/src/main/java/info/nightscout/androidaps/watchfaces/WatchfaceZone.java
@@ -0,0 +1,12 @@
+package info.nightscout.androidaps.watchfaces;
+
+public enum WatchfaceZone {
+    CHART,
+    BACKGROUND,
+    TOP,
+    DOWN,
+    CENTER,
+    LEFT,
+    RIGHT,
+    NONE
+}
