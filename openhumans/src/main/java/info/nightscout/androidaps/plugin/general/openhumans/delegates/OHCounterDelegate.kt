cb0c34fa470353e7d49ba31eb7d479afe6600fab
Philoul <legoupil.jp@free.fr>
2021-09-10 23:04:06 +0200

    Trial to check if crash is here

--- openhumans/src/main/java/info/nightscout/androidaps/plugin/general/openhumans/delegates/OHCounterDelegate.kt
+++ openhumans/src/main/java/info/nightscout/androidaps/plugin/general/openhumans/delegates/OHCounterDelegate.kt
@@ -1,6 +1,7 @@
 package info.nightscout.androidaps.plugin.general.openhumans.delegates
 
 import info.nightscout.androidaps.utils.sharedPreferences.SP
+import java.lang.Exception
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.reflect.KProperty
@@ -9,7 +10,12 @@ import kotlin.reflect.KProperty
 internal class OHCounterDelegate @Inject internal constructor(
     private val sp: SP
 ) {
-    private var value = sp.getLong("openhumans_counter", 1)
+    private var value = try {
+            sp.getLong("openhumans_counter", 1)
+        } catch(e:Exception) {
+            sp.putLong("openhumans_counter", 1)
+            1L
+        }
 
     operator fun getValue(thisRef: Any?, property: KProperty<*>): Long = value
 
