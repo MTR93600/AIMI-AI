fcffe18797ac4f138e15c7201877f3d52da8de12
osodebailar <a.eggen@gmail.com>
2022-03-04 10:50:42 +0100

    test code

--- core/src/main/java/info/nightscout/androidaps/utils/alertDialogs/OKDialog.kt
+++ core/src/main/java/info/nightscout/androidaps/utils/alertDialogs/OKDialog.kt
@@ -1,15 +1,49 @@
 package info.nightscout.androidaps.utils.alertDialogs
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.SystemClock
 import android.text.Spanned
+import android.view.Gravity
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import info.nightscout.androidaps.core.R
 import info.nightscout.androidaps.extensions.runOnUiThread
+import info.nightscout.androidaps.utils.AppUtils
+import info.nightscout.androidaps.utils.BlurView
 
 object OKDialog {
+
+    private var alert: AlertDialog? = null
+
+    fun setAlertDialogBackground(context: Context, result: Bitmap?) {
+        val activity = context as Activity
+        val draw = BitmapDrawable(activity.resources, result)
+        val window = alert?.window
+        window?.setBackgroundDrawable(draw)
+        window?.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        window?.setGravity(Gravity.CENTER)
+        alert?.show()
+        alert!!.setCanceledOnTouchOutside(false)
+    }
+
+    fun blur(activity: Activity) {
+        // apply blur effect
+        Thread(Runnable {
+            AppUtils.screenShot(activity){bitmap ->
+                val blurBitmap: Bitmap? = BlurView(activity.application).blurBackground(bitmap, 10)
+                runOnUiThread {
+                    setAlertDialogBackground( activity, blurBitmap)
+                }
+            }
+        }).start()
+    }
+
 
     @SuppressLint("InflateParams")
     fun show(context: Context, title: String, message: String, runnable: Runnable? = null) {
@@ -17,10 +51,11 @@ object OKDialog {
         var notEmptyTitle = title
         if (notEmptyTitle.isEmpty()) notEmptyTitle = context.getString(R.string.message)
 
-        AlertDialogHelper.Builder(context, R.style.DialogTheme)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(context, notEmptyTitle))
-            .setMessage(message)
-            .setPositiveButton(context.getString(R.string.ok)) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(context, R.style.DialogTheme)
+        with(builder){
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(context, notEmptyTitle))
+            setMessage(message)
+            setPositiveButton(context.getString(R.string.ok)) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -29,8 +64,12 @@ object OKDialog {
                     runOnUiThread(runnable)
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     @SuppressLint("InflateParams")
@@ -39,10 +78,11 @@ object OKDialog {
         var notEmptyTitle = title
         if (notEmptyTitle.isEmpty()) notEmptyTitle = activity.getString(R.string.message)
 
-        AlertDialogHelper.Builder(activity, R.style.DialogTheme)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, notEmptyTitle))
-            .setMessage(message)
-            .setPositiveButton(activity.getString(R.string.ok)) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(activity, R.style.DialogTheme)
+        with(builder){
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, notEmptyTitle))
+            setMessage(message)
+            setPositiveButton(activity.getString(R.string.ok)) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -51,8 +91,12 @@ object OKDialog {
                     runnable?.let { activity.runOnUiThread(it) }
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     fun showConfirmation(activity: FragmentActivity, message: String, ok: Runnable?) {
@@ -66,10 +110,11 @@ object OKDialog {
     @SuppressLint("InflateParams")
     fun showConfirmation(activity: FragmentActivity, title: String, message: Spanned, ok: Runnable?, cancel: Runnable? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(activity, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, title))
-            .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(activity, R.style.DialogTheme)
+        with(builder){
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, title))
+            setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -78,7 +123,7 @@ object OKDialog {
                     ok?.let { activity.runOnUiThread(it) }
                 }
             }
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -87,17 +132,22 @@ object OKDialog {
                     cancel?.let { activity.runOnUiThread(it) }
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     @SuppressLint("InflateParams")
     fun showConfirmation(activity: FragmentActivity, title: String, message: String, ok: Runnable?, cancel: Runnable? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(activity, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, title))
-            .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(activity, R.style.DialogTheme)
+        with(builder) {
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(activity, title))
+            setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -106,7 +156,7 @@ object OKDialog {
                     ok?.let { activity.runOnUiThread(it) }
                 }
             }
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -115,8 +165,12 @@ object OKDialog {
                     cancel?.let { activity.runOnUiThread(it) }
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     fun showConfirmation(context: Context, message: Spanned, ok: Runnable?, cancel: Runnable? = null) {
@@ -126,19 +180,20 @@ object OKDialog {
     @SuppressLint("InflateParams")
     fun showConfirmation(context: Context, title: String, message: Spanned, ok: Runnable?, cancel: Runnable? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(context, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
-            .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
-                if (okClicked) return@setPositiveButton
-                else {
-                    okClicked = true
-                    dialog.dismiss()
-                    SystemClock.sleep(100)
-                    runOnUiThread(ok)
+        val builder = AlertDialogHelper.Builder(context, R.style.DialogTheme)
+        with(builder) {
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
+                .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
+                    if (okClicked) return@setPositiveButton
+                    else {
+                        okClicked = true
+                        dialog.dismiss()
+                        SystemClock.sleep(100)
+                        runOnUiThread(ok)
+                    }
                 }
-            }
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -147,8 +202,12 @@ object OKDialog {
                     runOnUiThread(cancel)
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     fun showConfirmation(context: Context, message: String, ok: Runnable?, cancel: Runnable? = null) {
@@ -158,10 +217,11 @@ object OKDialog {
     @SuppressLint("InflateParams")
     fun showConfirmation(context: Context, title: String, message: String, ok: Runnable?, cancel: Runnable? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(context, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
-            .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(context, R.style.DialogTheme)
+        with(builder){
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
+            setPositiveButton(android.R.string.ok) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -170,7 +230,7 @@ object OKDialog {
                     runOnUiThread(ok)
                 }
             }
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -179,17 +239,22 @@ object OKDialog {
                     runOnUiThread(cancel)
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     @SuppressLint("InflateParams")
     fun showConfirmation(context: Context, title: String, message: String, ok: DialogInterface.OnClickListener?, cancel: DialogInterface.OnClickListener? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(context, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
-            .setPositiveButton(android.R.string.ok) { dialog: DialogInterface, which: Int ->
+        val builder = AlertDialogHelper.Builder(context, R.style.DialogTheme)
+        with(builder){
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
+            setPositiveButton(android.R.string.ok) { dialog: DialogInterface, which: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -198,7 +263,7 @@ object OKDialog {
                     ok?.onClick(dialog, which)
                 }
             }
-            .setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, which: Int ->
+            setNegativeButton(android.R.string.cancel) { dialog: DialogInterface, which: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -207,17 +272,23 @@ object OKDialog {
                     cancel?.onClick(dialog, which)
                 }
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
     @SuppressLint("InflateParams")
     fun showYesNoCancel(context: Context, title: String, message: String, yes: Runnable?, no: Runnable? = null) {
         var okClicked = false
-        AlertDialogHelper.Builder(context, R.style.DialogTheme)
-            .setMessage(message)
-            .setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
-            .setPositiveButton(R.string.yes) { dialog: DialogInterface, _: Int ->
+        val builder = AlertDialogHelper.Builder(context, R.style.DialogTheme)
+
+        with(builder){
+            setMessage(message)
+            setCustomTitle(AlertDialogHelper.buildCustomTitle(context, title))
+            setPositiveButton(R.string.yes) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setPositiveButton
                 else {
                     okClicked = true
@@ -226,7 +297,7 @@ object OKDialog {
                     runOnUiThread(yes)
                 }
             }
-            .setNegativeButton(R.string.no) { dialog: DialogInterface, _: Int ->
+            setNegativeButton(R.string.no) { dialog: DialogInterface, _: Int ->
                 if (okClicked) return@setNegativeButton
                 else {
                     okClicked = true
@@ -235,11 +306,15 @@ object OKDialog {
                     runOnUiThread(no)
                 }
             }
-            .setNeutralButton(R.string.cancel) { dialog: DialogInterface, _: Int ->
+            setNeutralButton(R.string.cancel) { dialog: DialogInterface, _: Int ->
                 dialog.dismiss()
             }
-            .show()
-            .setCanceledOnTouchOutside(false)
+        }
+        val alert = builder.create()
+        with( alert ){
+            show()
+            setCanceledOnTouchOutside(false)
+        }
     }
 
 }
