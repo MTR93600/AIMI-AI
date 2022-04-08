de1ffaf69d162c7ab80bbb23c69b1f71287116ab
DICKO <mark@servecold.net>
2022-04-08 18:30:14 +1000

    sensitivityRatio for sens_TDD 1 overnight

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -429,7 +429,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         sens_normalTarget = round(sens_normalTarget, 1);
         enlog += "sens_normalTarget now "+sens_normalTarget+ "due to temp target; ";
     } else {
-        sensitivityRatio = TDD/tdd24h;
+        sensitivityRatio = (eatingnowtimeOK && profile.use_sens_TDD ? TDD/tdd24h : 1);
         sensitivityRatio = (!profile.use_sens_TDD && typeof autosens_data !== 'undefined' && autosens_data ? autosens_data.ratio : sensitivityRatio);
         if (sensitivityRatio > 1) {
             sensitivityRatio = Math.min(sensitivityRatio, profile.autosens_max);
