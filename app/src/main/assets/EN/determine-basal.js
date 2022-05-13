7fa79af0180c3f6c62c9f6f530e6eb1ed39f56b9
DICKO <mark@servecold.net>
2022-05-13 18:41:46 +1000

    sens_target_bg stronger scale for COBBoost or UAM > 1.05 with sufficient delta

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -517,7 +517,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // calculate default ISF scaling first
     // within COBBoost window MAX 45 mins set slightly lower target for ISF scaling to handle rises from below target
-    var sens_target_bg = (COBBoostOK && cTime <=45 ? Math.max(target_bg-9,80) : target_bg);
+    var sens_target_bg = (COBBoostOK && cTime <=45 || meal_data.mealCOB > 0 && delta > 4 && DeltaPct > 1.05 ? Math.max(target_bg-9,80) : target_bg);
     var sens_BGscaler = (Math.log(Math.min(bg,ISFbgMax)/sens_target_bg)+1);
 
     // Convert ISFBGscaler to %
