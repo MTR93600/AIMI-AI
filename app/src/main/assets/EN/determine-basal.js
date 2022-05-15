7a1b321e88ea75c2a2088f14b5ea526d93da56cf
DICKO <mark@servecold.net>
2022-05-16 07:45:57 +1000

    sens_future active with TBR at night, restricted with SMB at night by autosens_max

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -980,14 +980,14 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
         sens_future_bg = Math.min(bg,eventualBG);
         sens_future_bg = Math.max(sens_future_bg,40); // minimum 2.2
         sens_eBGweight = (eventualBG < bg ? 1 : 0);
-        sens_future = sens_normalTarget / (sens_future_bg / target_bg);
-        // limit sens_future to autosens max
-        sens_future = Math.max(sens_future, sens_normalTarget/profile.autosens_max);
+        // apply dynamic ISF scaling formula
+        var sens_future_scaler = Math.log(sens_future_bg/target_bg)+1;
+        sens_future = sens_normalTarget/sens_future_scaler;
+        // limit sens_future to autosens max at night with SMB
+        sens_future = (!eatingnowtimeOK && bg < SMBbgOffset ? sens_future : Math.max(sens_future, sens_normalTarget/profile.autosens_max);
         // set sens_future_max to true for reason asterisk
         sens_future_max = (sens_future == sens_normalTarget/profile.autosens_max);
     }
-
-
 
     sens_future = round(sens_future,1);
     enlog += "* sens_eBGweight:\n";
