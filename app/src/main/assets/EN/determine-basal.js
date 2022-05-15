87fcb9ec735dc37cc9f7d981760e0005fa84c5ac
DICKO <mark@servecold.net>
2022-05-15 19:54:32 +1000

    sens_target_bg not allowed overnight

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -496,6 +496,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     // calculate default ISF scaling first
     // within COBBoost window MAX 45 mins set slightly lower target for ISF scaling to handle rises from below target
     var sens_target_bg = (COBBoostOK && cTime <=45 || meal_data.mealCOB == 0 && delta > 4 && DeltaPct > 1.05 ? Math.max(target_bg-9,80) : target_bg);
+    // only allow adjusted ISF target when eatingnow time is OK or at night with TBR before SMB activated
+    sens_target_bg = (eatingnowtimeOK || !eatingnowtimeOK && bg < SMBbgOffset ? sens_target_bg : target_bg);
     var sens_BGscaler = (Math.log(Math.min(bg,ISFbgMax)/sens_target_bg)+1);
 
     // Convert ISFBGscaler to %
