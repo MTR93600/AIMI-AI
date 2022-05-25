d1a589522252907c7f5a0c35afb8f24632d2fc92
MTR93600 <tellier@pm.me>
2022-05-26 01:00:24 +0200

    AAPS-3.0.0.2-dev-k-AIMI V18 26/05/2022 Dia modification update milos l

--- app/src/main/assets/FullUAM/determine-basal.js
+++ app/src/main/assets/FullUAM/determine-basal.js
@@ -1285,7 +1285,7 @@ if (AIMI_UAM && AIMI_BreakFastLight && now >= AIMI_BL_StartTime && now <= AIMI_B
 
 }
         console.log("------------------------------");
-                console.log(" AAPS-3.0.0.2-dev-k-AIMI V18 22/05/2022 ");
+                console.log(" AAPS-3.0.0.2-dev-k-AIMI V18 26/05/2022 ");
                 console.log("------------------------------");
                 if ( meal_data.TDDAIMI3 ){
                 console.log(enlog);
