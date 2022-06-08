396164e780e379aca01bd31b8076fc6bbec25d34
dicko72 <mark@servecold.net>
2022-06-08 13:07:14 +1000

    Threshold safety lifted after EN Window

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -734,7 +734,8 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // min_bg of 90 -> threshold of 65, 100 -> 70 110 -> 75, and 130 -> 85
     //var threshold = Math.max(min_bg - 0.5*(min_bg-40),72); // minimum 72
-    var threshold = Math.max(min_bg-0.5*(min_bg-40), profile.normal_target_bg-9, 75); // minimum 75 or current profile target - 10
+//    var threshold = Math.max(min_bg-0.5*(min_bg-40), profile.normal_target_bg-9, 75); // minimum 75 or current profile target - 10
+    var threshold = (ENWindowOK ? Math.max(min_bg-0.5*(min_bg-40),75) : Math.max(profile.normal_target_bg-9, 75)); // minimum 75 or current profile target - 10
 
     //console.error(reservoir_data);
 
