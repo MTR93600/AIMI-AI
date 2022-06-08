fe31f5132d946e6c77edf237475d6c0ce2ba17ac
dicko72 <mark@servecold.net>
2022-06-08 20:24:15 +1000

    sens_target_bg stronger when above SMBbgOffset

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -555,7 +555,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     // for delta > 4 and 105% change from short_avg or within COB window MAX 45 mins use lower target for ISF scaling
     // for all times when EN time is OK use 20% higher target for less ISF scaling
     // var sens_target_bg = c;
-    var sens_target_bg = (ENWindowOK ? ins_val : ins_val * 1.2);
+    var sens_target_bg = (ENWindowOK || bg > SMBbgOffset ? ins_val : ins_val * 1.2);
     // only allow adjusted ISF target when eatingnow time is OK and bg below ISFbgMax, dont use at night
     //sens_target_bg = (ENtimeOK && bg < ISFbgMax ? sens_target_bg : target_bg);
     sens_target_bg = (ENactive ? sens_target_bg : target_bg);
