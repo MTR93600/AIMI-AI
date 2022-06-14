2497b453a68e1388fe5d1f212cc69a2b2b6f890b
DICKO <mark@servecold.net>
2022-06-14 22:15:09 +1000

    TIR_sens

--- app/src/main/assets/EN/determine-basal.js
+++ app/src/main/assets/EN/determine-basal.js
@@ -429,7 +429,7 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
 
     // TIR
     var TIR_sens = 1;
-    if (meal_data.TIRW2 < meal_data.TIRW1 && Math.max(meal_data.TIRW1L, meal_data.TIRW2L)) == 0 { // if the 2nd hour TIR window is less in range and there are no lows
+    if (meal_data.TIRW2 < meal_data.TIRW1 && Math.max(meal_data.TIRW1L, meal_data.TIRW2L) == 0) { // if the 2nd hour TIR window is less in range and there are no lows
         TIR_sens = 1.15;
     }
     ins_val = ins_val / TIR_sens;
