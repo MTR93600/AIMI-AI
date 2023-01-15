465c0d72f665e33f4b5f3c211c53aec5ae363247
mtr93600 <tellier@pm.me>
2023-01-16 00:39:37 +0700

    modification de du ratio pour le b30 après un bolus si BIOF est actif

--- plugins/aps/src/main/assets/AIMI/b30mssvaimi/determine-basal.js
+++ plugins/aps/src/main/assets/AIMI/b30mssvaimi/determine-basal.js
@@ -476,11 +476,11 @@ var determine_basal = function determine_basal(glucose_status, currenttemp, iob_
     }
 
     if (iTimeActivation === true && iTime < (profile.b30_duration*1.618) && meal_data.countBolus === 1 && BFIOB===true && glucose_status.delta > 0){
-        rT.reason += ". force basal because iTime is running and lesser than "+(basal*1.618)+" minutes :"+(basal*10/60)*(profile.b30_duration*1.618)+" U, remaining time : " +((profile.b30_duration*1.618) - iTime);
+        rT.reason += ". force basal because iTime is running and lesser than "+(basal*1.618)+" minutes :"+(basal*(LastManualBolus/basal)/60)*(profile.b30_duration*1.618)+" U, remaining time : " +((profile.b30_duration*1.618) - iTime);
         //rT.deliverAt = deliverAt;
         rT.temp = 'absolute';
         rT.duration = (profile.b30_duration*1.618);
-        rate = round_basal(basal*10,profile);
+        rate = round_basal(basal*(LastManualBolus/basal),profile);
         rT.rate = rate;
         //round(((meal_data.TDDLastI3)/60)*20,2) > profile.current_basal*5 ? round(profile.current_basal*5,2) : round(((meal_data.TDDLastI3)/60)*20,2) ;
         //rT.rate = profile.current_basal*10;
