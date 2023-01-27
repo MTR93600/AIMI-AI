36be7d7a57814b7288988aefcb6cf50da5a68f6f
mtr93600 <tellier@pm.me>
2023-01-27 15:06:38 +0700

    change le delta pour aimi_delta dans new smb

--- plugins/aps/src/main/assets/AIMI/b30mssvaimi/determine-basal.js
+++ plugins/aps/src/main/assets/AIMI/b30mssvaimi/determine-basal.js
@@ -1820,11 +1820,11 @@ if (AIMI_UAM && AIMI_BreakFastLight && nowdec >= AIMI_BL_StartTime && nowdec <= 
             }else if (profile.key_use_newsmb){
              var bfl_bfiob = AIMI_BreakFastLight === true || BFIOB === true ? true : false;
              if (iTimeActivation && bfl_bfiob === true && !profile.temptargetSet && delta > 0 &&  aimismb === true && sens_predType == "UAM+" && UAMpredBG >= 220){
-             insulinReq = ((1 + Math.sqrt(delta)) / 4);
+             insulinReq = ((1 + Math.sqrt(aimi_delta)) / 4);
              var microBolus = circadian_smb > (-10) ? Math.min(AIMI_UAM_CAP,insulinReq*smb_ratio) : Math.min(AIMI_UAM_CAP,insulinReq);
              microBolus = (microBolus > (max_iob - iob_data.iob) ? (max_iob - iob_data.iob) : microBolus);
              }else if (iTimeActivation && !AIMI_BreakFastLight && !profile.temptargetSet && delta > 0 && aimismb === true && UAMpredBG >= 150){
-               insulinReq = ((1 + Math.sqrt(delta)) / 2);
+               insulinReq = ((1 + Math.sqrt(aimi_delta)) / 2);
                    if (circadian_smb > (-3)){
                      var microBolus = Math.min(AIMI_UAM_CAP,insulinReq*smb_ratio);
                      microBolus = (microBolus > (max_iob - iob_data.iob) ? (max_iob - iob_data.iob) : microBolus);
